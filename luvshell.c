473a29c8cb38cf428381fc55f9f4ba45cbbe692a
HellBee <63785221+ClassicHellBee@users.noreply.github.com>
2024-03-15 15:33:30 +0100

    v1.1.0

--- luvshell.c
+++ luvshell.c
@@ -30,12 +30,30 @@ void cd_command(char *args[]) {
     }
 }
 
+void export_command(char *args[]) {
+    if (args[1] != NULL && args[2] != NULL) {
+        setenv(args[1], args[2], 1);
+    } else {
+        printf("[!]: export VARIABLE\n");
+    }
+}
+
 void execute_command(char *args[], bool background) {
     pid_t pid = fork();
     if (pid == -1) {
         perror("fork");
         exit(EXIT_FAILURE);
     } else if (pid == 0) {
+        // ignora la regla SIGINT (Ctrl+C) en el proceso
+        signal(SIGINT, SIG_IGN);
+
+        // ejecuta el comando
+        execvp(args[0], args);
+
+        // si execvp devuelve, hay un error
+        perror("execvp");
+        exit(EXIT_FAILURE);
+        /*
         // redireccion de entrada y salida si es necesario
         int in_fd, out_fd;
         if (args[2] != NULL && strcmp(args[2], "<") == 0) {
@@ -59,31 +77,110 @@ void execute_command(char *args[], bool background) {
         execvp(args[0], args);
         perror("execvp");
         exit(EXIT_FAILURE);
+*/
     } else {
         if (!background) {
             int status;
             waitpid(pid, &status, 0);
+        } else {
+            printf("[%d] %s running in background\n", pid, args[0]);
         }
     }
 }
 
-/*
-void expand_wildcards(char *args[]) {
-    glob_t glob_result;
-    int i = 0;
-    while (args[i] != NULL) {
-        if (strpbrk(args[i], "*?[]") != NULL) {
-            glob(args[i], GLOB_NOCHECK | GLOB_TILDE, NULL, &glob_result);
-            for (int j = 0; j < glob_result.gl_pathc; j++) {
-                args[i + j] = strdup(glob_result.gl_pathv[j]);
-            }
-            args[i + glob_result.gl_pathc] = NULL;
-            globfree(&glob_result);
+int main() {
+    char command[MAX_COMMAND_LENGTH];
+
+    // manejo de la regla (Ctrl+C)
+    signal(SIGINT, sigint_handler);
+
+    while(1) {
+        printf("luvShell$: ");
+
+        // leer la entrada del usuario
+        fgets(command, sizeof(command), stdin);
+        command[strcspn(command, "\n")] = 0;
+        if (strcmp(command, "exit") == 0) {
+            break;
         }
-        i++;
+
+        // tokenizar la entrada del usuario
+        char *commands[MAX_ARGS];
+        char *token = strtok(command, "|");
+        int num_commands = 0;
+        while (token != NULL && num_commands < MAX_ARGS - 1) {
+            commands[num_commands++] = token;
+            token = strtok(NULL, "|");
+        }
+        commands[num_commands] = NULL;
+
+        // ejecucion de comandos en tuberia
+        int fd[2];
+        int in_fd = STDIN_FILENO;
+        for (int i = 0; i < num_commands; i++) {
+            char *args[MAX_ARGS];
+            token = strtok(commands[i], " ");
+            int j = 0;
+            bool background = false;
+            while (token != NULL && j < MAX_ARGS - 1) {
+                if (strcmp(token, "&") == 0) {
+                    background = true;
+                    break;
+                }
+                args[j++] = token;
+                token = strtok(NULL, " ");
+            }
+            args[j] = NULL;
+
+            if (strcmp(args[0], "cd") == 0) {
+                cd_command(args);
+                continue;
+            } else if (strcmp(args[0], "export") == 0) {
+                export_command(args);
+                continue;
+            }
+
+            pipe(fd);
+
+            pid_t pid = fork();
+            if(pid == -1) {
+                perror("fork");
+                exit(EXIT_FAILURE);
+            } else if (pid == 0) {
+                dup2(in_fd, STDIN_FILENO);
+                close(fd[0]);
+                if (i < num_commands -1) {
+                    dup2(fd[1], STDOUT_FILENO);
+                }
+                close(fd[1]);
+
+                // ejecuta el comando
+                execute_command(args, background);
+
+                // si execute_command devuelve, hay un error
+                exit(EXIT_FAILURE);
+            } else {
+                close(fd[1]);
+                if (in_fd != STDIN_FILENO) {
+                    close(in_fd);
+                }
+                in_fd = fd[0];
+                if (!background) {
+                    int status;
+                    waitpid(pid, &status, 0);
+                } else {
+                    printf("[%d] %s running in background\n", pid, args[0]);
+                }
+            }
+        }
+
+        // esperar a q terminen todos los procesos en segundo plano
+        while (waitpid(-1, NULL, WNOHANG) > 0);
     }
+
+    return 0;
 }
-*/
+/*
 
 int main() {
     char command[MAX_COMMAND_LENGTH];
@@ -121,9 +218,9 @@ int main() {
             // verifica si se trata de un comando interno o no
             if (strcmp(args[0], "cd") == 0) {
                 cd_command(args);
+            } else if (strcmp(args[0], "export") == 0) {
+                export_command(args);
             } else {
-                // expandir las wildcards en los argumentos
-                // expand_wildcards(args);
                 // ejecutar comando externo
                 execute_command(args, background);
             }
@@ -133,25 +230,4 @@ int main() {
     return 0;
 }
 
-/*
-        if (pid == -1) {
-            perror("fork");
-            exit(EXIT_FAILURE);
-        }
-        else if (pid == 0) {
-            char *args[] = {"/bin/sh", "-c", command, NULL};
-            execvp(args[0], args);
-
-            // si execvp retorna, hubo un perror
-            perror("execvp");
-            exit(EXIT_FAILURE);
-        }
-        else {
-            int status;
-            waitpid(pid, &status, 0);
-        }
-    }
-
-    return 0;
-}
 */
